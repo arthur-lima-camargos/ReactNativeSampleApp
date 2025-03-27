17f0435881891220985ad7f2e42ba15984b58a8b
Henrique <henrique.ladeira@dtidigital.com.br>
2025-03-27 17:55:17 -0300

    update

--- screens/arcs.tsx
+++ screens/arcs.tsx
@@ -1,4 +1,4 @@
-import { Button, FlatList, Text, View } from "react-native";
+import { Button, FlatList, Text, TextInput, View } from "react-native";
 import { FallbackWrapper } from "@/components/FallbackWrapper";
 import {
   getAllArcsManager,
@@ -14,8 +14,17 @@ import {
   LinearTransition,
 } from "react-native-reanimated";
 import { ArcItem } from "@/components/ArcItem";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 export const ArcsScreen = () => {
+  const inputRef = useRef<TextInput>(null);
+  const inputRef2 = useRef<TextInput>(null);
+  const scrollRef = useRef<FlatList>(null);
+  const inputText = useRef("");
+
+  const valor = useRef(0);
+  const [val, setVal] = useState(0);
+
   const { data, refetch, ...status } = useQuery({
     queryKey: ["arc"],
     queryFn: getAllArcsManager,
@@ -37,6 +46,14 @@ export const ArcsScreen = () => {
       queryClient.invalidateQueries({ queryKey: [StorageApi.favoriteArcKey] }),
   });
 
+  useEffect(() => {
+    console.log("renderizou");
+  });
+
+  const onPress = () => {
+    scrollRef?.current?.scrollToEnd();
+  };
+
   return (
     <View style={{ flex: 1, padding: 20, backgroundColor: "#B0E0E6" }}>
       {favoriteArc && (
@@ -44,12 +61,30 @@ export const ArcsScreen = () => {
           Favorite Arc: {favoriteArc.title}
         </Text>
       )}
+      <TextInput
+        ref={inputRef}
+        style={{ height: 100 }}
+        placeholder="Input"
+        onChangeText={(text) => {
+          inputText.current = text;
+        }}
+        onEndEditing={() => {
+          inputRef2?.current?.focus();
+        }}
+      />
+      <TextInput ref={inputRef2} style={{ height: 100 }} placeholder="Input2" />
+      <Button title="asd" onPress={onPress} />
       <FallbackWrapper
         {...status}
         isLoading={status.isFetching}
         refetch={refetch}
       >
         <FlatList
+          ref={scrollRef}
+          onScroll={(a) => {
+            setVal(a.nativeEvent.contentOffset.y);
+            console.log(val);
+          }}
           data={data}
           renderItem={({ item, index }) => (
             <ArcItem
